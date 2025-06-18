568cf424f47274d8980834b278a084ec79ba04fe
nbielano <parr0nj6@gmail.com>
2025-06-18 10:25:28 -0400

    Delete const.py

--- const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for the light_log_test integration."""
-
-DOMAIN = "nb_light_test"
-ATTR_NAME = "name"
-DEFAULT_NAME = "Nick"
-ATTR_COLOR = "RGB Color"
-DEFAULT_COLOR = [255, 0, 0]
-ATTR_ENTITY_ID = "light.sengled_e21_n1ea"
